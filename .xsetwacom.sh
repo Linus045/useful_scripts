c38dff831d0e68312d3a9e01b7a6964d80512119
Linus045 <lwillms88@gmail.com>
2023-05-23 20:33:22 +0200

    Updated .xsetwacom to autocamically detect correct monitor name

--- .xsetwacom.sh
+++ .xsetwacom.sh
@@ -8,10 +8,22 @@ PAD="HID 256c:006e Pad pad"
 STYLUS="HID 256c:006e stylus"
 STRIP="HID 256c:006e Touch Strip pad"
 
+
+
 # small delay to wait for all devices to be registered when called from udev rule
 sleep 2
 
-xsetwacom set "$STYLUS" MapToOutput HDMI-A-0
+# gets the name of the first monitor containing HDMI 
+# reverse the string, cut by spaces, grab first element and reverse string again to get the name of the first connected monitor
+MAIN_MONITOR=$(xrandr --listactivemonitors | grep HDMI | rev | cut -d' ' -f1 | rev)
+if [[ -n $MAIN_MONITOR ]]; then
+  # grab first monitor you can find if no hdmi monitor is connected
+  # print only second line
+  # reverse the string, cut by spaces, grab first element and reverse string again to get the name of the first connected monitor
+  MAIN_MONITOR=$(xrandr --listactivemonitors | sed -n "2{p;q}" | rev | cut -d' ' -f1 | rev)
+fi
+
+xsetwacom set "$STYLUS" MapToOutput $MAIN_MONITOR
 xsetwacom set "$STRIP" Button 1 "3"
 
 xsetwacom set "$PAD" "Button" "1" "key ctrl z" # Button 5
