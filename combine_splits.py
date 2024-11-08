56644c2a513863694685d912ab8bec7a709b6767
Linus045 <lwillms88@gmail.com>
2024-11-08 11:26:48 +0100

    Update combine_splits.py

--- combine_splits.py
+++ combine_splits.py
@@ -23,11 +23,13 @@ def last_6chars(x):
     return (x[-6:])
 
 
+parts_to_delete = []
 for filename, parts in filenames.items():
     command = ["cat"]
     parts = sorted(parts, key=last_6chars)
     for part in parts:
         command.append(part)
+        parts_to_delete.append(part)
 
     print(command)
     with open(filename, "w") as outputFile:
@@ -35,7 +37,10 @@ for filename, parts in filenames.items():
 
     print(f"Combined parts into {filename}")
 
-    for part in parts:
+print("Please validate that everything works as expected")
+input = input("Remove part files? [y/N]: ")
+if input == 'y' or input == "Y":
+    for part in parts_to_delete:
         if os.path.exists(part):
             os.remove(part)
         else:
