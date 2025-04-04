eb1ac68ac8bcf69646cd29672f4eaccefcbb7ef5
Linus045 <linuswillms00@gmail.com>
2025-04-04 18:16:53 +0200

    fix: split_files script now also copies files smaller than 100MB if its a .cbz file

--- split_large_files.sh
+++ split_large_files.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 mkdir -p splits
-find . -size +100M | xargs -I{} bash -c 'split -a 6 -b 100M "{}" "./splits/$(basename "{}")_____"'
+
+find . -maxdepth 1 -size +100M -iname '*.cbz' | xargs -I{} bash -c 'split -a 6 -b 100M "{}" "./splits/$(basename "{}")_____"'
+find . -maxdepth 1 -size -100M -iname '*.cbz' | xargs -I{} bash -c 'cp "{}" splits/'
